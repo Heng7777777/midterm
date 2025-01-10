f4bc301b6d57f0dbe7c9293655003e57ad736568
UN Bunheng <91281601+Heng7777777@users.noreply.github.com>
2025-01-10 11:56:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 # 2. inputs
-Seat_comfort = st.number_input(label="Seat comfort", value=1.0)
-Inflight_wifi_service = st.number_input(label="Inflight wifi service", value=2.0)
-Inflight_entertainment = st.number_input(label="Inflight entertainment", value=4.0)
-Online_support = st.number_input(label="Online support", value=3.0)
-Ease_of_Online_booking = st.number_input(label="Ease of Online booking", value=2.0)
-On_board_service = st.number_input(label="On-board service", value=1.0)
-Leg_room_service = st.number_input(label="Leg room service", value=3.0)
-Baggage_handling = st.number_input(label="Baggage handling", value=1.0)
-Checkin_service = st.number_input(label="Checkin service", value=1.0)
-Cleanliness = st.number_input(label="Cleanliness", value=1.0)
-Online_boarding = st.number_input(label="Online boarding", value=1.0)
+Seat_comfort = st.number_input(label="Seat comfort (rate: 1-5)", value=1)
+Inflight_wifi_service = st.number_input(label="Inflight wifi service (rate: 1-5)", value=2)
+Inflight_entertainment = st.number_input(label="Inflight entertainment (rate: 1-5)", value=4)
+Online_support = st.number_input(label="Online support (rate: 1-5)", value=3)
+Ease_of_Online_booking = st.number_input(label="Ease of Online booking (rate: 1-5)", value=2)
+On_board_service = st.number_input(label="On-board service (rate: 1-5)", value=1)
+Leg_room_service = st.number_input(label="Leg room service (rate: 1-5)", value=3)
+Baggage_handling = st.number_input(label="Baggage handling (rate: 1-5)", value=1)
+Checkin_service = st.number_input(label="Checkin service (rate: 1-5)", value=1)
+Cleanliness = st.number_input(label="Cleanliness (rate: 1-5)", value=1)
+Online_boarding = st.number_input(label="Online boarding (rate: 1-5)", value=1)
 
 # 3. Combine input into an array of X
 X_num = np.array(
